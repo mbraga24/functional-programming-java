145024e47234ae6ec2002b61857d47c4abc59c13
mrdbrg <rdg.brg@gmail.com>
2022-04-25 16:10:21 -0400

    sumList - use reduce()

---
+++ src/programming/FP07Exercise.java
@@ -0,0 +1,27 @@
+package programming;
+
+import java.util.List;
+
+public class FP07Exercise {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		
+		int sum = addList(numbers);
+		
+		System.out.println(sum);
+		
+	}
+	
+	private static int sum(int a, int b) {
+		return a + b;
+	}
+	
+	private static int addList(List<Integer> numbers) {
+		
+		return numbers.stream()
+		.reduce(0, FP07Exercise ::sum);
+	
+	}
+
+}
